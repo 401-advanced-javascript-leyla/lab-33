68152df343e9a7f8c5f3390425a2384198056a18
leyla li <leylali@qq.com>
2019-09-27 17:42:30 -0700

    done with coding and documentation

---
+++ src/components/counter/CounterProvider.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const CounterContext = React.createContext();
+
+class CounterProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      increment: this.increment,
+      decrement: this.decrement,
+      styles: this.styles,
+    };
+  }
+
+  styles = {
+    container: {
+      alignItems: 'center',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      width: '40%',
+    },
+    counter: {
+      display: 'inline-block',
+      fontSize: '1em',
+      border: '2px solid #444',
+      borderRadius: '50%',
+      lineHeight: '5em',
+      textAlign: 'center',
+      width: '5em',
+    },
+    button: {
+      height: '3em',
+    },
+  };
+
+  increment = () => {
+    this.setState((previousState) => (
+      { count: previousState.count + 1 }
+    ));
+  };
+
+  decrement = () => {
+    this.setState((previousState) => (
+      { count: previousState.count - 1 }
+    ));
+  };
+
+  render() {
+    return (
+      <CounterContext.Provider value = {this.state}>
+        {this.props.children}
+      </CounterContext.Provider>
+
+    );
+  }
+}
+
+export default CounterProvider;
+
+CounterProvider.propTypes = {
+  children: PropTypes.object,
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './app.js';
+import App from './app';
+import ConterContext from './components/counter/CounterProvider';
+
 
 class Main extends React.Component {
   render() {
-    return <App />;
+    return (
+      <>
+        <ConterContext>
+          <App />
+        </ConterContext>
+      </>
+    );    
   }
 }
 
